8d874ae606a96775b4c2ac0391b5d8c4b1eff657
Scott Garcia <45701000+scottdgarciajr@users.noreply.github.com>
2024-10-17 12:26:16 -0400

    fixed config for images for depployment

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import esbuild from 'rollup-plugin-esbuild';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'index.html',
@@ -19,6 +20,11 @@ export default {
     /** Enable using HTML as rollup entrypoint */
     html({
       minify: true,
+    }),
+    copy({/**added to make images work in deployment */
+      targets: [
+        { src: 'lib/', dest: 'dist' }
+      ]
     }),
     /** Resolve bare module imports */
     nodeResolve(),
