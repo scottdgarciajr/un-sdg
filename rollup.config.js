bc38a35b6c5a7bbb290b83f2c52670b9b43d304b
Scott Garcia <45701000+scottdgarciajr@users.noreply.github.com>
2024-10-17 12:28:52 -0400

    fixed dist issue

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default {
     }),
     copy({/**added to make images work in deployment */
       targets: [
-        { src: 'lib/', dest: 'dist' }
+        { src: 'lib/', dest: 'public' }
       ]
     }),
     /** Resolve bare module imports */
